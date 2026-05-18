711a22187ac8dde94bd80fe37a81f7013b8b8284
talonlikeaclaw <talonlikeaclaw@proton.me>
2026-05-18 16:49:57 -0400

    Update home.packages with developer tool definitions

--- home/default.nix
+++ home/default.nix
@@ -10,19 +10,45 @@
   programs.home-manager.enable = true;
 
   home.packages = with pkgs; [
-    bar
+    bat
+    beekeeper-studio
+    bitwarden-desktop
+    bruno
+    cargo
     curl
+    docker
     eza
     fastfetch
     fd
+    filezilla
     fzf
+    gcc
+    gnumake
+    go
     htop
     jq
     lazygit
+    librewolf
+    llama-cpp
+    llama-swap
+    ncdu
+    nodejs_24
+    obsidian
+    opencode
+    openssl
+    pkg-config
     ripgrep
+    rustc
+    shellcheck
+    starship
+    television
     tldr
     tree
+    unzip
+    wezterm
     wget
+    yazi
+    zed-editor
     zoxide
   ];
 
