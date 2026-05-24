cabc1a8b5cf90a8216e5ebf624a701ddb88fca6a
talonlikeaclaw <talonlikeaclaw@proton.me>
2026-05-24 17:51:50 -0400

    Add zed configuration to home-manager

--- home/default.nix
+++ home/default.nix
@@ -29,6 +29,7 @@ in
     ./starship.nix
     ./television.nix
     ./wezterm.nix
+    ./zed.nix
     ./zsh.nix
   ];
 
---
+++ home/zed.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+
+{
+  home.sessionVariables.ZED_ALLOW_EMULATED_GPU = "1";
+  xdg.configFile."zed/settings.json" = {
+    source = ./zed/settings.json;
+  };
+
+  xdg.configFile."zed/keymap.json" = {
+    source = ./zed/keymap.json;
+  };
+
+  xdg.configFile."zed/tasks.json" = {
+    source = ./zed/tasks.json;
+  };
+}
