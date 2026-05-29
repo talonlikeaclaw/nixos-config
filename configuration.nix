f4ae4c3c62245e7a978d3b472c850ea5ba95ed41
talonlikeaclaw <talonlikeaclaw@proton.me>
2026-05-29 14:04:52 -0400

    Update boot loader settings to proxmox environment

--- configuration.nix
+++ configuration.nix
@@ -11,9 +11,9 @@
     ];
 
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/vda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
