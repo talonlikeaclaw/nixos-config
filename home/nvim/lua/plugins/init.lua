52f34d46da2cd7fdad5ed94ea47837e06eb9d77a
talonlikeaclaw <talonlikeaclaw@proton.me>
2026-05-29 14:05:05 -0400

    Remove toggleterm plugin and keymaps/config

--- home/nvim/lua/plugins/init.lua
+++ home/nvim/lua/plugins/init.lua
@@ -7,7 +7,6 @@ vim.pack.add({
   { src = 'https://github.com/christoomey/vim-tmux-navigator', name = 'vim-tmux-navigator' },
   { src = 'https://github.com/MeanderingProgrammer/render-markdown.nvim', name = 'render-markdown.nvim' },
   { src = 'https://github.com/obsidian-nvim/obsidian.nvim', name = 'obsidian.nvim' },
-  { src = 'https://github.com/akinsho/toggleterm.nvim', name = 'toggleterm.nvim' },
   { src = 'https://github.com/rafamadriz/friendly-snippets', name = 'friendly-snippets' },
 })
 
@@ -36,6 +35,5 @@ require('plugins.splitjoin')
 require('plugins.surround')
 require('plugins.markdown')
 require('plugins.obsidian')
-require('plugins.toggleterm')
 require('plugins.sessions')
 require('plugins.treesitter')
