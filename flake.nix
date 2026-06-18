d774235b17d5ebb758cd9a836ad3af8b5685c5f0
talonlikeaclaw <talonlikeaclaw@proton.me>
2026-06-18 19:29:48 -0400

    Add devbox to flake.nix config

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
 
   outputs = { nixpkgs, home-manager, plasma-manager, ... }: {
     nixosConfigurations = {
+      # GUI VM
       vm = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
 	modules = [
@@ -27,6 +28,20 @@
 	    home-manager.users.talon = import ./home/default.nix;
 	    home-manager.backupFileExtension = "hm-bak";
 	    home-manager.sharedModules = [ plasma-manager.homeModules.plasma-manager ];
+	  }
+	];
+      };
+
+      # Headless Devbox
+      devbox = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+	modules = [
+	  ./devbox.nix
+	  ./devbox-hardware.nix
+	  home-manager.nixosModules.home-manager
+	  {
+	    home-manager.users.talon = import ./home/devbox.nix;
+	    home-manager.backupFileExtension = "hm-bak";
 	  }
 	];
       };
